deb794c30c89ce2904a93c51e9c87c9082380be1
danfxs <danielson_fx@yahoo.com.br>
2024-02-16 15:43:21 -0300

    Redux simples completed

---
+++ redux-simples/src/components/Soma.jsx
@@ -0,0 +1,27 @@
+import './Intervalo.css'
+import React from 'react';
+import Card from './Card';
+import { connect } from 'react-redux';
+
+function Soma(props) {
+    const {min, max} = props
+    return (
+        <Card title="Soma dos Números" blue>
+            <div>
+                <span>
+                    <span>Resultado:</span>
+                    <strong>{max + min}</strong>
+                </span>
+            </div>
+        </Card>
+    );
+}
+
+function mapStateToProps(state) {
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+export default connect(mapStateToProps)(Soma);
